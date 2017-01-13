1fe6d7f3553fa4f65b0f95ecc24bff4d5d657580
kade-robertson <kade.robertson96@gmail.com>
2017-01-13 14:27:37 -0500

    set up themeinfo

--- SharpThemes/Objects/ThemeInfo.cs
+++ SharpThemes/Objects/ThemeInfo.cs
@@ -4,12 +4,25 @@ namespace SharpThemes.Objects
 {
     class ThemeInfo 
     {
-        public readonly int Top;
-        public readonly int TopF;
-        public readonly int Bot;
-        public readonly int BotF;
-        public readonly int Fol;
-        public readonly int Bor;
-        public readonly int SFX;
+        [JsonProperty(PropertyName = "top")]
+        public int Top { get; }
+
+        [JsonProperty(PropertyName = "topf")]
+        public int TopF { get; }
+
+        [JsonProperty(PropertyName = "bot")]
+        public int Bot { get; }
+
+        [JsonProperty(PropertyName = "botf")]
+        public int BotF { get; }
+
+        [JsonProperty(PropertyName = "fol")]
+        public int Fol { get; }
+
+        [JsonProperty(PropertyName = "bor")]
+        public int Bor { get; }
+
+        [JsonProperty(PropertyName = "sfx")]
+        public int SFX { get; }
     }
 }
