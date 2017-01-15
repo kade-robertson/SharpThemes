c16f8666622049c327f5c392ca7c83eb898942da
Kade <kade-robertson@users.noreply.github.com>
2017-01-14 23:13:15 -0500

    actual spacefix

--- SharpThemes/Utilities/Http.cs
+++ SharpThemes/Utilities/Http.cs
@@ -5,17 +5,17 @@ using ModernHttpClient;
 
 namespace SharpThemes.Utilities
 {
-	public static class Http
-	{
-		public static async Task<string> DoGet(string url) {
-			using (var client = new HttpClient(new NativeMessageHandler())) {
-				try {
-					return await client.GetStringAsync(url);	
-				} catch (Exception ex) {
-					throw ex;
-				}
-			}
+    public static class Http
+    {
+	public static async Task<string> DoGet(string url) {
+	    using (var client = new HttpClient(new NativeMessageHandler())) {
+		try {
+			return await client.GetStringAsync(url);	
+		} catch (Exception ex) {
+			throw ex;
 		}
+	    }
 	}
+    }
 }
 
