c091e64ed63f457ffc5e5201ba521ab9ce19f250
Kade Robertson <kade.robertson96@gmail.com>
2017-01-14 23:05:05 -0500

    basic http

---
+++ SharpThemes/Utilities/Http.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Net.Http;
+using System.Threading.Tasks;
+using ModernHttpClient;
+
+namespace SharpThemes.Utilities
+{
+	public static class Http
+	{
+		public static async Task<string> DoGet(string url) {
+			using (var client = new HttpClient(new NativeMessageHandler())) {
+				return await client.GetStringAsync(url);	
+			}
+		}
+	}
+}
+
