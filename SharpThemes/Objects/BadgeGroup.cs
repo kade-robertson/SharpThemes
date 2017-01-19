e3a53bbde5971d544ee7388e5e9a614f05570f8c
kade-robertson <kade.robertson96@gmail.com>
2017-01-19 14:58:29 -0500

    prelim badge objects

---
+++ SharpThemes/Objects/BadgeGroup.cs
@@ -0,0 +1,21 @@
+﻿using Newtonsoft.Json;
+using System.Collections.Generic;
+
+namespace SharpThemes.Objects
+{
+    public class BadgeGroup 
+    {
+        [JsonProperty(PropertyName = "count")]
+        public uint BadgeCount { get; set; }
+
+        [JsonProperty(PropertyName = "themes")]
+        public List<Badge> Badges { get; set; }
+
+        [JsonProperty(PropertyName = "userPrivelege")]
+        public string UserPriveledge { get; set; }
+
+        public override string ToString() {
+            return JsonConvert.SerializeObject(this, Formatting.Indented);
+        }
+    }
+}
