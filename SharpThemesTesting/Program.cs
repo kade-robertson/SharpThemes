c9a646c5efdc4acc2599c77fcfe1762bd7ba13b1
kade-robertson <kade.robertson96@gmail.com>
2017-01-20 11:44:08 -0500

    travis integration

--- SharpThemesTesting/Program.cs
+++ SharpThemesTesting/Program.cs
@@ -11,14 +11,19 @@ namespace SharpThemesTesting
     class Program
     {
         static void Main(string[] args) {
-            var test = Task.Run(() => ThemeClient.SearchThemes(username: "Rocket_Grunt")).Result;
+            Console.Title = "SharpThemes Demo";
+            Console.WriteLine(@"          _____ __                   ________                            ");
+            Console.WriteLine(@"         / ___// /_  ____ __________/_  __/ /_  ___  ____ ___  ___  _____");
+            Console.WriteLine(@"         \__ \/ __ \/ __ `/ ___/ __ \/ / / __ \/ _ \/ __ `__ \/ _ \/ ___/");
+            Console.WriteLine(@"        ___/ / / / / /_/ / /  / /_/ / / / / / /  __/ / / / / /  __(__  ) ");
+            Console.WriteLine(@"       /____/_/ /_/\__,_/_/  / .___/_/ /_/ /_/\___/_/ /_/ /_/\___/____/  ");
+            Console.WriteLine(@"                            /_/ A demo application for SharpThemes       ");
+            Console.WriteLine(@"--------------------------------------------------------------------------------");
+            var test = Task.Run(() => ThemeClient.GetMostRecentThemes()).Result;
             Console.WriteLine("Most recent themes:");
             foreach (Theme t in test.Themes)
             {
-                Console.WriteLine($" - {t.Name} [Downloads: {t.Downloads}]");
-                Console.WriteLine($"    - Download: {t.DownloadURL}");
-                Console.WriteLine($"    - Preview: {t.PreviewImageURL}");
-                Console.WriteLine($"    - Background Music: {t.BackgroundMusicURL}");
+                Console.WriteLine($" - {t.Name} [Downloads: {t.Downloads}]" + (t.IsNSFW ? " [NSFW]" : "") + $" by {t.CreatedBy}");
             }
             Console.Read();
         }
