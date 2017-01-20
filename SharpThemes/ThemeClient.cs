ec75ddf8c8d87c57ca8c6ec8c22f1237e00b0bcc
kade-robertson <kade.robertson96@gmail.com>
2017-01-20 11:13:06 -0500

    more generics, rearranging files

--- SharpThemes/ThemeClient.cs
+++ SharpThemes/ThemeClient.cs
@@ -12,15 +12,19 @@ namespace SharpThemes
         private static string m_badgebaseurl = "https://badges.3dsthem.es/getThemes.php";
         private static string m_splashbaseurl = "https://splash.3dsthem.es/getThemes.php";
 
+        private static string GetBaseURL<T>() {
+            if (typeof(T) == typeof(Theme)) {
+                return m_themebaseurl;
+            } else if (typeof(T) == typeof(Badge)) {
+                return m_badgebaseurl;
+            }
+            return m_themebaseurl;
+        }
+
         private async static Task<X> GenericSearch<T, X>(string text, SortBy sorting, string username) {
             var urlb = new StringBuilder();
 
-            if (typeof(T) == typeof(Theme)) {
-                urlb.Append(m_themebaseurl);
-            } else if (typeof(T) == typeof(Badge)) {
-                urlb.Append(m_badgebaseurl);
-            }
-
+            urlb.Append(GetBaseURL<T>());
             urlb.Append("?q=");
 
             if (text != string.Empty) {
@@ -33,26 +37,42 @@ namespace SharpThemes
                 urlb.Append("user:" + username);
             }
 
-            try {
-                var data = await Http.DoGet(urlb.ToString());
-                return JsonConvert.DeserializeObject<X>(data);
-            } catch {
-                throw new System.Exception(urlb.ToString());
-            }
+            var data = await Http.DoGet(urlb.ToString());
+            return JsonConvert.DeserializeObject<X>(data);
+        }
+
+        private async static Task<X> GenericTopList<T, X>(TopList t) {
+            var urlb = new StringBuilder();
+
+            urlb.Append(GetBaseURL<T>());
+            urlb.Append(t.ToString());
+
+            var data = await Http.DoGet(urlb.ToString());
+            return JsonConvert.DeserializeObject<X>(data);
         }
 
         public async static Task<ThemeGroup> SearchThemes(string text = "", SortBy sorting = default(SortBy), string username = "") {
             return await GenericSearch<Theme, ThemeGroup>(text, sorting, username);
         }
 
+        public async static Task<BadgeGroup> SearchBadges(string text = "", SortBy sorting = default(SortBy), string username = "") {
+            return await GenericSearch<Badge, BadgeGroup>(text, sorting, username);
+        }
+
         public async static Task<ThemeGroup> GetMostRecentThemes() {
-            var data = await Http.DoGet(string.Concat(m_themebaseurl, "?recent"));
-            return JsonConvert.DeserializeObject<ThemeGroup>(data);
+            return await GenericTopList<Theme, ThemeGroup>(TopList.MostRecent);
+        }
+
+        public async static Task<BadgeGroup> GetMostRecentBadges() {
+            return await GenericTopList<Badge, BadgeGroup>(TopList.MostRecent);
         }
 
         public async static Task<ThemeGroup> GetMostPopularThemes() {
-            var data = await Http.DoGet(string.Concat(m_themebaseurl, "?popular"));
-            return JsonConvert.DeserializeObject<ThemeGroup>(data);
+            return await GenericTopList<Theme, ThemeGroup>(TopList.MostPopular);
+        }
+
+        public async static Task<BadgeGroup> GetMostPopularBadges() {
+            return await GenericTopList<Badge, BadgeGroup>(TopList.MostPopular);
         }
     }
 }
---
+++ SharpThemes/TopList.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace SharpThemes
+{
+    public class TopList 
+    {
+        private string text = "?recent";
+
+        public static readonly TopList MostPopular = new TopList("?popular");
+        public static readonly TopList MostRecent = new TopList("?recent");
+
+        private TopList(string text) {
+            this.text = text;
+        }
+
+        public override string ToString() {
+            return text.ToString();
+        }
+    }
+}
